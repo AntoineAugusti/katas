e1f691bfcf385dfa3f79998eda250002215b3f29
Antoine Augusti <antoine.augusti@gmail.com>
2022-12-04 14:53:37 +0100

    Day 3 part 2

--- adventofcode/2022/03.exs
+++ adventofcode/2022/03.exs
@@ -20,6 +20,19 @@ input
     MapSet.new(b |> String.split("", trim: true))
   )
 end)
-|> Enum.map(&(&1 |> MapSet.to_list() |> hd() |> to_int.()))
+|> Enum.map(&(&1 |> Enum.at(0) |> to_int.()))
 |> Enum.sum()
+|> IO.inspect()
+
+input
+|> Enum.chunk_every(3)
+|> Enum.reduce(0, fn records, sum ->
+  records
+  |> Enum.reduce(records |> hd() |> String.split("", trim: true) |> MapSet.new(), fn el, acc ->
+    MapSet.intersection(acc, el |> String.split("", trim: true) |> MapSet.new())
+  end)
+  |> Enum.at(0)
+  |> to_int.()
+  |> Kernel.+(sum)
+end)
 |> IO.inspect()
